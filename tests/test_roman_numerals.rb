8f45a12ba1226ecbf8d69e342cdeadca7e689223
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-04 14:50:49 +0900

    rewrite test code by using Rspec

--- tests/test_roman_numerals.rb
+++ tests/test_roman_numerals.rb
@@ -1,8 +1,8 @@
-require 'minitest/autorun'
+# require 'minitest/autorun'
 require_relative '../src/roman_numerals'
 
-class TestRomanNumerals < Minitest::Test
-    def test_single_digit
-        assert_equal 'I', to_roman(1)
+RSpec.describe 'An example group' do
+    it 'single symbol test' do
+      expect('I').to eq(to_roman(1))
     end
 end
